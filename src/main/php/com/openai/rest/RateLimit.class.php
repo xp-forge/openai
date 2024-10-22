56f6d6741fd42b6efc9ea4ac16d7413aa693bf1c
Timm Friebe <t.friebe@enbw.com>
2024-10-22 19:58:05 +0200

    Implement lang.Value

--- src/main/php/com/openai/rest/RateLimit.class.php
+++ src/main/php/com/openai/rest/RateLimit.class.php
@@ -1,6 +1,28 @@
 <?php namespace com\openai\rest;
 
+use lang\Value;
+
 /** @see https://platform.openai.com/docs/guides/rate-limits/rate-limits-in-headers */
-class RateLimit {
+class RateLimit implements Value {
   public $remaining= null;
+
+  /** @return string */
+  public function toString() {
+    return nameof($this).'(remaining: '.(null === $this->remaining ? '(n/a)' : $this->remaining).')';
+  }
+
+  /** @return string */
+  public function hashCode() {
+    return 'R'.$this->remaining;
+  }
+
+  /**
+   * Comparison
+   *
+   * @param  var $value
+   * @return int
+   */
+  public function compareTo($value) {
+    return $value instanceof self ? $this->remaining <=> $value->remaining : 1;
+  }
 }
